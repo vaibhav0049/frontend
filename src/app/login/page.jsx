d8894de1fb2b46cc048313751ed546f71dc9f0c9
Vaibhav0049 <kumar.vaibhav2004@bbdu.ac.in>
2024-02-26 23:31:26 +0530

    creating multi pages website using nextjs

---
+++ src/app/login/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './login.module.css'
+
+const Login = () => {
+  return (
+    <div> Login
+     <h1 className= {classes.myBtn}> login pagge </h1>
+    </div>
+  )
+}
+
+export default Login;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,29 @@
 // rafce  code template of nextjs
 import React from 'react'
 
-const home  = () => {
+const Home  = () => {
+
+  const siteTitle='vaibhav';
+  const num1=10;
+  const num2=30;
+
+
+
+  const addnums =(a,b)=> {
+      return a+b ;
+  }
   return (
     <div>
-       <h1> heloo world!</h1>
-       <img src="https://in.images.search.yahoo.com/images/view;_ylt=AwrKF4tvOdtlVV0EF3a9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzY3NTU0YjM0MTM0NzYyOTc3MjNlNGJlZWU0NTkwZDY1BGdwb3MDMgRpdANiaW5n?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dreact%26type%3DE211IN826G0%26fr%3Dmcafee%26fr2%3Dpiv-web%26tab%3Dorganic%26ri%3D2&w=1200&h=630&imgurl=arnondora.in.th%2Fcontent%2Fimages%2F2021%2F01%2Fhow-i-get-started-with-react-logo-og.png&rurl=https%3A%2F%2Farnondora.in.th%2Fhow-i-get-started-with-react%2F&size=10.5KB&p=react&oid=67554b3413476297723e4beee4590d65&fr2=piv-web&fr=mcafee&tt=How+I+Get+Started+With+React+-+Arnondora&b=0&ni=21&no=2&ts=&tab=organic&sigr=F7BjOa5mOwb1&sigb=CeCcCERPP7Vv&sigi=sNFEU38YXVw5&sigt=yT7LpISa5QaN&.crumb=b/XugW9PBce&fr=mcafee&fr2=piv-web&type=E211IN826G0"/>
+    {siteTitle}
+    <h2 className='my-btn'>{num1+num2}</h2>
+
+    
+    <p> {addnums(987,987)}</p>
+       <h1  style={{color :'red', fontSize:'3rem'}}> heloo world!</h1>
+       <img src= "https://images.squarespace-cdn.com/content/v1/51a8f870e4b02509ec24213a/1575352696924-HEUAOA7R8DN15EMFFS2V/ke17ZwdGBToddI8pDm48kPqQfq0L3n3wpHIsRapTfg8UqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKczo5Zn4xktlpMsMj-QlHXeMfNK6GwvtVkYEWiR8XAPyD3GfLCe_DXOSC_YcAacWL_/CBDD97EF-651F-4148-A917-2F53223BB01F.jpeg" />
        <p> lorem34</p>
     </div>
   )
 }
 
-export default home ;
+export default Home ;
