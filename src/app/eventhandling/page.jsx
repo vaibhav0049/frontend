40f907897861b699b083579022316d5ed8228b0e
Vaibhav0049 <kumar.vaibhav2004@bbdu.ac.in>
2024-02-29 18:35:54 +0530

    using onlcik function

--- src/app/eventhandling/page.jsx
+++ src/app/eventhandling/page.jsx
@@ -7,7 +7,7 @@ const EventHandling = () => {
      <h1 className='text-center'> Event Handling</h1>
      <hr/>
       <button className='btn btn-primary' onClick={  () =>{ alert('dabaaaa diya ')}}> Dabayiee NAA </button>
-      <input type='color' onChange={(e ) => { document.body.style.background = e.target.value }}/>
+      
     </div>
   )
 }
